270d2adcb27b322f92ba1b69bc587bbacc5899dc
fernanda rodriguez <fernandarpy@gmail.com>
2023-02-02 16:06:04 -0500

    implementacion item

--- src/components/ItemCities/ItemCities.jsx
+++ src/components/ItemCities/ItemCities.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./itemCities.css";
-import LocationIconBlack from "../../assets/locationIconBlack.svg"
+import LocationIconBlack from "../../assets/locationIconBlack.svg";
 
 const ItemCities = () => {
-  return(
+  return (
     <div className="container_item">
-<img src={LocationIconBlack} alt="imglocation" />
-        <p className="container_item_ciudad">Ciudad</p>
-        <p className="container_item_pais">Pais</p>
+      <div className="container_item_img">
+        <img src={LocationIconBlack} alt="location" />
+      </div>
+      <div className="container_item_descripcion">
+      <p className="container_item_ciudad">Ciudad</p>
+      <p className="container_item_pais">Pais</p>
     </div>
-  )
+    </div>
+  );
 };
 
 export default ItemCities;
