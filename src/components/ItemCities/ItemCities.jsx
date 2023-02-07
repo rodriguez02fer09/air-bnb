496fd1c269d5656857479dc3e50749f0618d6983
fernanda rodriguez <fernandarpy@gmail.com>
2023-02-07 11:07:25 -0500

    styles item

--- src/components/ItemCities/ItemCities.jsx
+++ src/components/ItemCities/ItemCities.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import "./itemCities.css";
 import LocationIconBlack from "../../assets/locationIconBlack.svg";
 
-const ItemCities = ({city , country}) => {
+const ItemCities = ({ city, country }) => {
   return (
-    <div className="container_item">
-      <div className="container_item_img">
-        <img src={LocationIconBlack} alt="location" />
+    
+   
+      <div className="container_item">
+        <div className="container_item_img">
+          <img src={LocationIconBlack} alt="location" />
+        </div>
+        <div className="container_item_descripcion">
+          <p className="container_item_ciudad">{city}</p>
+          <p className="container_item_pais">{country}</p>
+        </div>
       </div>
-      <div className="container_item_descripcion">
-        <p className="container_item_ciudad">{city}</p>
-        <p className="container_item_pais">{country}</p>
-      </div>
-    </div>
+    
+   
   );
 };
 
 export default ItemCities;
-
